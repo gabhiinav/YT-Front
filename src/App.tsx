26e82851dace80ac1ed122e23d365ef7420669a0
gabhiinav <gabhiinav@gmail.com>
2024-04-12 08:19:09 +0530

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-function App() {
+import { useState } from "react";
+import { CategoryPills } from "./components/CategoryPills";
+import { categories, videos } from "./data/home";
+import { PageHeader } from "./components/PageHeader";
+import { VideoGridItem } from "./components/VideoGridItem";
+import { Sidebar } from "./components/Sidebar";
+import { SidebarProvider } from "./contexts/SidebarContext";
+
+export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">YouTube</h1>
-    </>
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CategoryPills
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map((video) => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
   );
 }
-
-export default App;
---
+++ src/data/sidebar.ts
@@ -0,0 +1,46 @@
+export const subscriptions = [
+  {
+    channelName: "Juxtopposed",
+    id: "Juxtopposed",
+    imgUrl:
+      "https://yt3.ggpht.com/6SUvLvQ6tJ8l6PAS3Y9OdsN7l-VVXVCsBhYqBcfvpfogaScX-3PoHVqq4mgbutsX-l-miiYgVA=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "WolfeyVGC",
+    id: "WolfeyVGC",
+    imgUrl:
+      "https://yt3.googleusercontent.com/OOXxZnMYS5zFjUiyFcUuPeiXGD3EqcmYznmybj4vxH1qcXRQgwovXxzN_3QRn1__CzWvb0_pAA=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Real Science",
+    id: "RealScience",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/AIdro_laXkwz2N1CloK3nQjouWj7uXLDgtXUbbmqlGpY7dDxFA=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Web Dev Simplified",
+    id: "WebDevSimplified",
+    imgUrl:
+      "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "Marques Brownlee",
+    id: "mkbhd",
+    imgUrl:
+      "https://yt3.googleusercontent.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "GlitchxCity",
+    id: "GlitchxCity",
+    imgUrl:
+      "https://yt3.googleusercontent.com/zLi0vtocp5ChuVnshs0KDVEmsaxQ7M4qo2IVY9tQcnvXk2fRyYO71isMXlG-uiRhI7ZeWi4Ia4E=s176-c-k-c0x00ffffff-no-rj",
+  },
+];
+
+export const playlists = [
+  { id: "1", name: "Favorites" },
+  { id: "2", name: "Technology" },
+  { id: "3", name: "Design" },
+  { id: "4", name: "Pokemon" },
+  { id: "5", name: "Dev" },
+];
